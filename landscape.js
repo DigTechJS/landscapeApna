bfa9d50bb8bfc241d5e9f1cf04f4995d42bd1e94
Shreya <shreya11072003@gmail.com>
2022-05-17 18:43:07 +0530

    change

--- landscape.js
+++ landscape.js
@@ -1,186 +1,213 @@
+document.addEventListener('DOMContentLoaded',function(){
+    const outerRect= document.getElementById("outer");
+    const terrain= document.getElementById("terrain");
+    const MAX_WIDTH= +outerRect.getAttributeNS(null,'width');
+    const MIN_HEIGHT= +terrain.querySelector('rect').getAttributeNS(null,'y');
+    const MAX_HEIGHT= +outerRect.getAttributeNS(null,'height');
+    const pineElement=document.querySelector('#pine');
+    const oak=document.querySelector('#oak');
+    const treeButtons=document.getElementById('treeButtons');
+    treeGenerator.init(outerRect,terrain,MAX_WIDTH,MIN_HEIGHT,MAX_HEIGHT,pineElement,oak);
+    treeButtons.addEventListener('click',function(e){
+        switch(e.target.id){
+            case 'addOak':
+                treeGenerator.createOaks();
+                break;
+            case 'pineButton':
+                treeGenerator.createPineTrees();
+                break;
+            case 'removeTrees':
+                treeGenerator.removeAlltrees();
+                break;
+            default:
+                return;
+        }
+    })
+
+    document.getElementById('save').addEventListener('click',function(e){
+        const {format}=e.target.dataset;
+        if(format){
+            const svgData=document.getElementById('svg').outerHTML;
+            const name=document.getElementById('fileName').value;
+            imageSaver.save(name,format,svgData);
+        }
+    });
+    
+});
 
 
-const ns="http://www.w3.org/2000/svg";
-const outerRect= document.getElementById("outer");
-const terrain= document.getElementById("terrain");
-const MAX_WIDTH= +outerRect.getAttributeNS(null,'width');
-const MIN_HEIGHT= +terrain.querySelector('rect').getAttributeNS(null,'y');
-const MAX_HEIGHT= +outerRect.getAttributeNS(null,'height');
-const pineElement=document.querySelector('#pine path');
-const pineButton=document.getElementById('createPines');
-const oak=document.querySelector('#oak');
-const addOak=document.getElementById('addOak');
-const treeButtons=document.getElementById('treeButtons');
-let myTrees=[];
-treeButtons.addEventListener('click',treeDispatcher);
+treeGenerator={
+    init(outerRect,terrain,MAX_WIDTH,MIN_HEIGHT,MAX_HEIGHT,pineElement,oak){
+        this.outerRect=outerRect;
+        this.terrain=terrain;
+        this.MAX_WIDTH=MAX_WIDTH;
+        this.MIN_HEIGHT=MIN_HEIGHT;
+        this.MAX_HEIGHT=MAX_HEIGHT;
+        this.pineElement=pineElement;
+        this.oak=oak;
+        this.myTrees=[];
+        this.treeButtons=treeButtons;
 
-function treeDispatcher(e){
-    switch(e.target.id){
+    },
 
-        case 'pineButton':
-            createPineTrees();
-            break;
-        case 'removeTrees':
-            removeAlltrees();
-            break;
-        case 'addOak':
-            createOaks();
-            break;
-        default:
-            return;
+    createOaks(){
+        for(let j=0;j<20;j++){
+            const newElement= this.oak.cloneNode(true);
+            newElement.removeAttributeNS(null,'id');
+            newElement.setAttributeNS(null,'class','generatedOak');
+            const x=this.getRandom(this.MAX_WIDTH,1);
+            const y=this.getRandom(this.MAX_HEIGHT,this.MIN_HEIGHT);
+            this.setSVGAttributes(newElement,{class:'generatedOak','data-yaxis':y})
+            const rect=newElement.getElementsByTagName('rect')[0];
+            newElement.setAttributeNS(null,'data-yAxis',y);
+            this.setSVGAttributes(rect,{x:x,y:y});
+            const ellipses=newElement.getElementsByTagName('ellipse');
+            for(let i=0;i<ellipses.length;i++){
+                this.setSVGAttributes(ellipses[i],{cx:((i+1)*10)+x-20,cy:this.getRandom(y-10,y+10)})
+            }
+            this.myTrees.push(newElement);
+            }
+            // console.log(this.myTrees);
+            const sortedTrees=this.orderByYAxis(this.myTrees);
+            this.appendTreesInOrder(sortedTrees);
+
+    },
+
+
+    createPineTrees(){
+        // for(let i=0; i<20;i++){
+        //     const pineClone=this.pineElement.cloneNode();
+        //     const x=this.getRandom(this.MAX_WIDTH,1)+30;
+        //     const y=this.getRandom(this.MAX_HEIGHT,this.MIN_HEIGHT)+30;
+        //     const randomWidth=this.getRandom(100,50);
+        //     const rect=pineClone.getElementsByTagName('rect')[0];
+        //     this.setSVGAttributes(pineClone,{class:'generatesPine',d:`M${x},${y}, ${x-randomWidth},${y} ${x-randomWidth/2} ${y-randomWidth*1.5} ,Z`,'data-yAxis':y})
+        //     this.setSVGAttributes(rect,{x:x,y:y});
+        //     this.myTrees.push(pineClone);
+    
+        // }
+        // // console.log(this.myTrees);
+        // const sortedTrees=this.orderByYAxis(this.myTrees);
+        // this.appendTreesInOrder(sortedTrees);
+
+
+        for(let j=0;j<20;j++){
+            const newElement= this.pineElement.cloneNode(true);
+            // const pineClone=this.pineElement2.cloneNode();
+            newElement.removeAttributeNS(null,'id');
+            newElement.setAttributeNS(null,'class','generatesPine');
+            const x=this.getRandom(this.MAX_WIDTH,1);
+            const y=this.getRandom(this.MAX_HEIGHT,this.MIN_HEIGHT);
+            this.setSVGAttributes(newElement,{class:'generatedPine','data-yaxis':y})
+            const rect=newElement.getElementsByTagName('rect')[0];
+            const path=newElement.getElementsByTagName('path')[0];
+            newElement.setAttributeNS(null,'data-yAxis',y);
+            const randomWidth=this.getRandom(100,50);
+            this.setSVGAttributes(rect,{x:`${x-randomWidth/2}`,y:y});
+            this.setSVGAttributes(path,{d:`M${x},${y}, ${x-randomWidth},${y} ${x-randomWidth/2} ${y-randomWidth*1.5} ,Z`,'data-yAxis':y})
             
-
-    }
-
-}
-
-
-// addOak.addEventListener('click',createOaks);
-
-
-function setSVGAttributes(element,obj){
-    for(const prop in obj){
-        element.setAttributeNS(null,prop,obj[prop]);
-    }
-}
+            this.myTrees.push(newElement);
+            }
+            // console.log(this.myTrees);
+            const sortedTrees=this.orderByYAxis(this.myTrees);
+            this.appendTreesInOrder(sortedTrees);
+    },
 
 
-function createOaks(){
-    for(let j=0;j<20;j++){
+    removeAlltrees(){
+        for(const prop of this.myTrees){
+            this.terrain.removeChild(prop);
+            // myTrees.pop(prop);
+        }
+        this.myTrees=[];
+
+    },
+
+    appendTreesInOrder(trees){
+        for(const prop of trees){
+            this.terrain.appendChild(prop);
+        }
+    },
+
+    setSVGAttributes(element,obj){
+        for(const prop in obj){
+            element.setAttributeNS(null,prop,obj[prop]);
+        }
+    },
+
+    getRandom(max,min){
+        return Math.floor(Math.random() *(max-min+1))+ min;
+    },
+
+    orderByYAxis(trees){
+        const sortedTrees=trees.sort(function(a,b){
+            const ay=a.dataset.yAxis;
+            const by=b.dataset.yAxis;
+            if (ay>by) return 1;
+            else if (by>ay) return -1;
+            else return 0;
+            
+        });
+        // console.log(sortedTrees);
+        return sortedTrees;
+    },
 
     
-    const newElement= oak.cloneNode(true);
-    newElement.removeAttributeNS(null,'id');
-    newElement.setAttributeNS(null,'class','generatedOak');
-    const x=getRandom(MAX_WIDTH,1);
-    const y=getRandom(MAX_HEIGHT,MIN_HEIGHT);
-    setSVGAttributes(newElement,{class:'generatedOak','data-yaxis':y})
-    const rect=newElement.getElementsByTagName('rect')[0];
-    newElement.setAttributeNS(null,'data-yAxis',y);
-    setSVGAttributes(rect,{x:x,y:y});
-    // rect.setAttributeNS(null,'x',x);
-    // rect.setAttributeNS(null,'y',y);
-    // newElement.setAttributeNS(null,'data-yAxis',y);
-    const ellipses=newElement.getElementsByTagName('ellipse');
-    for(let i=0;i<ellipses.length;i++){
-        // ellipses[i].setAttributeNS(null,'cx',((i+1)*10)+x-20);
-        // ellipses[i].setAttributeNS(null,'cy',getRandom(y-10,y+10));
-        setSVGAttributes(ellipses[i],{cx:((i+1)*10)+x-20,cy:getRandom(y-10,y+10)})
-    }    
-
-    // terrain.appendChild(newElement);
-    myTrees.push(newElement);
-    }
-    console.log(myTrees);
-    orderByYAxis();
 }
 
-function orderByYAxis(){
-    const sortedTrees=myTrees.sort(function(a,b){
-        const ay=a.dataset.yAxis;
-        const by=b.dataset.yAxis;
-        if (ay>by) return 1;
-        else if (by>ay) return -1;
-        else return 0;
+const imageSaver={
+
+    save(name,format,svgData){
+        this.name=name;
+        this.format=format;
+        this.svgData=svgData;
+        this.parseImage();
+    },
+    parseImage(){
         
-    })
-    console.log(sortedTrees);
-    for(const prop of sortedTrees){
-        terrain.appendChild(prop);
-    }
-}
+        
+        if (this.name.length<1) {
+            this.name="unnamed";
+        }
+        const svgBlob= new Blob([this.svgData], {type: 'image/svg+xml'});
+        const svgURL=URL.createObjectURL(svgBlob);
+        if(this.format==='png'){
+            this.convertToPNG(svgURL)
+            .then(href=>this.downloadImg(href))    }
+        else{
+            this.downloadImg(svgURL);
+        }
+                
+    },
 
+    convertToPNG(svgURL){
+        return new Promise((resolve,reject) => {
+            const img= new Image();
+            img.src=svgURL;
+            img.onload=(()=>{
+            const canvas= document.createElement('canvas');
+            canvas.width=1520;
+            canvas.height=672;
+            const context=canvas.getContext('2d');
+            context.drawImage(img,0,0);
+            const href=canvas.toDataURL('image/png');
+            resolve(href);
+        })
+        })
+    },
 
-// pineButton.addEventListener('click',createPineTrees);
-
-function createPineTrees(){
-
-    for(let i=0; i<20;i++){
-        const pineClone=pineElement.cloneNode();
-        const x=getRandom(MAX_WIDTH,1)+30;
-        const y=getRandom(MAX_HEIGHT,MIN_HEIGHT)+30;
-        const randomWidth=getRandom(100,50);
-        setSVGAttributes(pineClone,{class:'generatesPine',d:`M${x},${y}, ${x-randomWidth},${y} ${x-randomWidth/2} ${y-randomWidth*1.5} ,Z`,'data-yAxis':y})
-        // pineClone.setAttributeNS(null,'class','generatedPine');
-        // pineClone.setAttributeNS(null,'d',`M${x},${y}, ${x-randomWidth},${y} ${x-randomWidth/2} ${y-randomWidth*1.5} ,Z`);
-        // pineClone.setAttributeNS(null,'data-yaxis',y);
-        // terrain.appendChild(pineClone);
-        myTrees.push(pineClone);
-
-    }
-    console.log(myTrees);
-    orderByYAxis();
-};
-
-function getRandom(max,min){
-    return Math.floor(Math.random() *(max-min+1))+ min;
-}
-
-// document.getElementById('removeTrees').addEventListener('click',removeAlltrees);
-
-function removeAlltrees(){
-    // const allPines=terrain.querySelectorAll('.generatedPine');
-    for(const prop of myTrees){
-        terrain.removeChild(prop);
-        // myTrees.pop(prop);
-    }
-    myTrees=[];
-}
-
-
-document.getElementById('save').addEventListener('click',saveImage);
-
-function saveImage(e){
-    const {format}=e.target.dataset;
-    if (format){
-        const svgData=document.getElementById('svg').outerHTML;
-    let name=document.getElementById('fileName').value;
-    if (name.length<1) {
-        name="unnamed";
-    }
-    const svgBlob= new Blob([svgData], {type: 'image/svg+xml;charset=utf-8'});
-    const svgURL=URL.createObjectURL(svgBlob);
-    if(format==='png'){
-        convertAndDownloadPNG(name,svgURL,format);
-
-    }
-    else{
-        downloadImg(name,format,svgURL);
-    }
-}
-    
-}
-
-
-function convertAndDownloadPNG(name,svgURL,format){
- 
-
-    const img= new Image();
-    img.src=svgURL;
-    img.onload=(()=>{
-        const canvas= document.createElement('canvas');
-        canvas.width=1682;
-        canvas.height=672;
-        const context=canvas.getContext('2d');
-        context.drawImage(img,0,0);
-        const href=canvas.toDataURL('image/png');
-        downloadImg(name,format,href);
-
-    })
-}
-
-function downloadImg(name,type,href){
-    const downloadLink=document.createElement('a');
+    downloadImg(href){
+        const downloadLink=document.createElement('a');
     downloadLink.href=href;
-    downloadLink.download=`${name}.${type}`;
+    downloadLink.download=`${this.name}.${this.format}`;
     // console.log(downloadLink);
-    downloadLink.onClick=(e)=>{
+    downloadLink.onClick=function(e){
         setTimeout(()=>{
             URL.revokeObjectURL(this.href);
-            console.log("revoked Obejct URL ");
         },1000)
     }
     document.body.appendChild(downloadLink);
     downloadLink.click();
     document.body.removeChild(downloadLink);
-}
+    }
+};
